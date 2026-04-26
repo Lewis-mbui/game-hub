5efe648dda5a36bfedec3cc0eceb0450871a9b04
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-26 13:32:01 +0300

    Refactoring: remove duplicate interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import type { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
-import type { Platform } from "./hooks/useGames";
+import type { Platform } from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
