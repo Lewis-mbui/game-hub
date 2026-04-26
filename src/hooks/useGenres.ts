05273c0f6b59d2220e361607ff7739999d5dfbc0
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-26 13:47:42 +0300

    Refactoring: creating a reusable API client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_GENRES } from "@/data/constants";
-import type { FetchResponse } from "@/services/api-client";
-import apiClient from "@/services/api-client";
+import APIClient from "@/services/api-client";
 import genres from "@/data/genres";
+
+const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -13,8 +14,7 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: CACHE_KEY_GENRES,
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
     initialData: { count: genres.length, results: genres },
   });
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,29 @@
-import axios, { CanceledError } from "axios";
+import axios, { CanceledError, type AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "a6c4c31bb05c4347be4f4444c2c1abd4",
   },
 });
 
-export { CanceledError };
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
