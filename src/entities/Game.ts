c696fae9af16c397ca4ec955a52dd1a4588abc4d
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-28 13:47:07 +0300

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
 import type { Platform } from "@/entities/Platform";
+import type { Genre } from "./Genre";
+import type { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
