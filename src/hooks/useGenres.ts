c4de93f8745f0866f277daa2749a575be49c7998
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-28 13:11:09 +0300

    Reafactor: move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,14 +3,9 @@ import { CACHE_KEY_GENRES } from "@/data/constants";
 import APIClient from "@/services/api-client";
 import genres from "@/data/genres";
 import ms from "ms";
+import type { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 const useGenres = () =>
   useQuery({
