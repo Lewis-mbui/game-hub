4f4e3f21b64cd63666f091f21da4b5e4664ed69a
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-28 12:33:13 +0300

    Handle errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import NavBar from "@/components/NavBar";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Heading fontSize={"2xl"}>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This Page does not exist."
+            : "An unexpected error occurred"}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
